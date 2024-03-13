4c79a8afcd655c552645aa021c2ddf158926d841
Abere Fejiro <aberefejiro@gmail.com>
2024-03-13 21:31:46 +0100

    base apparel project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "script.js"],
+  theme: {
+    extend: {
+      colors: {
+        "desaturated-red": "hsl(0, 36%, 70%)",
+        "soft-red": "hsl(0, 93%, 68%)",
+        "dark-grayish-red": "hsl(0, 6%, 24%)",
+      },
+      backgroundImage: () => ({
+        "gradient-to-neutral":
+          "linear-gradient(135deg, hsl(0, 0%, 100%), hsl(0, 100%, 98%))",
+        "gradient-to-primary":
+          "linear-gradient(135deg, hsl(0, 80%, 86%), hsl(0, 74%, 74%))",
+      }),
+    },
+  },
+  plugins: [],
+};
+
