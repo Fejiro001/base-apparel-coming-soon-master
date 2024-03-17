27c8027deae3fab0e75cc65cd6583a861907e335
Abere Fejiro <aberefejiro@gmail.com>
2024-03-17 23:51:09 +0100

    DONE!

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
+"use strict";
+
+const form = document.getElementById("form-element");
 const email = document.getElementById("email");
-const errorMessage = document.getElementById("error-message");
+const errorMessageElement = document.getElementById("error-message");
 const errorIcon = document.getElementById("error-icon");
 const submitEmail = document.getElementById("submit-email");
+
+const successMessage = "Successfully subscribed!";
+const errorMessage = "Please provide a valid email";
+
+// displays error message and icon depending on validity
+const displayError = () => {
+  errorMessageElement.style.display = "block";
+  errorIcon.style.display = "block";
+  errorMessageElement.textContent = errorMessage;
+  form.style.borderColor = "hsl(0, 93%, 68%)";
+  errorMessageElement.style.color = "hsl(0, 93%, 68%)";
+};
+
+submitEmail.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (email.validity.patternMismatch) {
+    displayError();
+  } else if (email.validity.valueMissing) {
+    displayError();
+  } else {
+    errorIcon.style.display = "none";
+    errorMessageElement.style.display = "block";
+    errorMessageElement.textContent = successMessage;
+    errorMessageElement.style.color = "green";
+    form.style.borderColor = "hsl(0, 36%, 70%)";
+  }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,9 +19,9 @@ module.exports = {
         "4xl": "clamp(2.25rem, 4.1vw, 4rem)",
         sm: "clamp(0.875rem, 1.1vw, 2rem)",
       },
-      backgroundImage: (theme) => ({
+      backgroundImage: () => ({
         "gradient-to-neutral":
-          "linear-gradient(135deg, hsl(0, 0%, 100%), hsl(0, 100%, 98%))",
+          "linear-gradient(135deg, hsl(0, 100%, 90%), hsl(0, 74%, 85%))",
         "gradient-to-primary":
           "linear-gradient(135deg, hsl(0, 80%, 86%), hsl(0, 74%, 74%))",
       }),
