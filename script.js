8d740e264c42e7280c74c5e6f720aaccebc0ede8
Abere Fejiro <aberefejiro@gmail.com>
2024-03-17 21:14:41 +0100

    done styling, time to add functionality

---
+++ script.js
@@ -0,0 +1,4 @@
+const email = document.getElementById("email");
+const errorMessage = document.getElementById("error-message");
+const errorIcon = document.getElementById("error-icon");
+const submitEmail = document.getElementById("submit-email");
